lyrra/MuseScore4

WIP: queue midi events towards jackaudiodriver

src/framework/audio/iaudiodriver.h
@@ -32,8 +32,13 @@
 #include "modularity/imoduleinterface.h"
 
 #include "audiotypes.h"
+#include "framework/midi/miditypes.h"
+#include "framework/audio/midiqueue.h"
 
 namespace mu::audio {
+
+class AudioDriverState;
+
 class IAudioDriver : MODULE_EXPORT_INTERFACE
 {
     INTERFACE_ID(IAudioDriver)
@@ -79,10 +84,12 @@ public:
 
     virtual std::vector<unsigned int> availableOutputDeviceBufferSizes() const = 0;
 
+    //virtual std::shared_ptr<ThreadSafeQueue<const mu::midi::Event>&> getMidiQueue() const = 0;
+    virtual bool pushMidiEvent(mu::midi::Event&) = 0;
+
     virtual void resume() = 0;
     virtual void suspend() = 0;
 };
-using IAudioDriverPtr = std::shared_ptr<IAudioDriver>;
 
 class AudioDriverState
 {
@@ -91,6 +98,7 @@ public:
     virtual bool open(const IAudioDriver::Spec& spec, IAudioDriver::Spec* activeSpec) = 0;
     virtual void close() = 0;
     virtual bool isOpened() const = 0;
+    virtual bool pushMidiEvent(mu::midi::Event&) = 0;
 
     IAudioDriver::Spec m_spec; // current running spec
     std::string m_deviceId;

diedeno/MuseScore

src/framework/audio/iaudiodriver.h
@@ -32,8 +32,12 @@
 #include "modularity/imoduleinterface.h"
 
 #include "audiotypes.h"
+#include "framework/midi/miditypes.h"
+#include "framework/audio/midiqueue.h"
 
 namespace muse::audio {
+class AudioDriverState;
+
 class IAudioDriver : MODULE_EXPORT_INTERFACE
 {
     INTERFACE_ID(IAudioDriver)
@@ -79,10 +83,11 @@ public:
 
     virtual std::vector<unsigned int> availableOutputDeviceBufferSizes() const = 0;
 
+    virtual bool pushMidiEvent(muse::midi::Event&) = 0;
+
     virtual void resume() = 0;
     virtual void suspend() = 0;
 };
-using IAudioDriverPtr = std::shared_ptr<IAudioDriver>;
 
 class AudioDriverState
 {
@@ -91,6 +96,7 @@ public:
     virtual bool open(const IAudioDriver::Spec& spec, IAudioDriver::Spec* activeSpec) = 0;
     virtual void close() = 0;
     virtual bool isOpened() const = 0;
+    virtual bool pushMidiEvent(muse::midi::Event&) = 0;
 
     IAudioDriver::Spec m_spec; // current running spec
     std::string m_deviceId;